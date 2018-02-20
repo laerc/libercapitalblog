695b691a24c9a3ac57b154f4b0b5792bf14b59a8
laerc <laercio@libercapital.com.br>
2018-02-20 19:30:11 -0300

    Adicionando Padrao Rubocop

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,52 +1,52 @@
+# This Class is the article controller
 class ArticlesController < ApplicationController
-	#include the module
-	include ArticlesHelper
+  # Include the module
+  include ArticlesHelper
 
-	def index
-		#All the articles that were created before are now at the articles variable
-  		@articles = Article.all
-	end
+  def index
+    # All the articles that were created before are now at the articles variable
+    @articles = Article.all
+  end
 
-	def show
-		#article has an article with an especific id.
-		@article = Article.find(params[:id])
+  def show
+    # article has an article with an especific id.
+    @article = Article.find(params[:id])
+  end
 
-	end
+  def new
+    @article = Article.new
+  end
 
-	def new
-		@article = Article.new
-	end
+  def create
+    # creates a new article
+    @article = Article.new
+    # takes the values
+    @article = Article.new(article_params)
 
-	def create
-		#creates a new article
-		@article = Article.new
-		#takes the values
-		@article = Article.new(article_params)
+    # pull to the database
+    @article.save
 
-  		#pull to the database
-  		@article.save
+    # redirects the page
+    redirect_to article_path(@article)
+  end
 
-  		#redirects the page
-  		redirect_to article_path(@article)
-	end	
+  def destroy
+    Article.find(params[:id]).destroy
+    redirect_to action: 'index'
+  end
 
-	def destroy
-		Article.find(params[:id]).destroy
-		redirect_to action:"index"
-	end
+  # creates an article to be updatted
+  def edit
+    @article = Article.find(params[:id])
+  end
 
-	#creates an article to be updatted
-	def edit
-  		@article = Article.find(params[:id])
-	end
+  # method that save de new configuration
+  def update
+    @article = Article.find(params[:id])
+    @article.update(article_params)
 
-	#method that save de new configuration
-	def update
-		@article = Article.find(params[:id])
-		@article.update(article_params)
+    flash.notice = "Article '#{@article.title}' Updated!"
 
-		flash.notice = "Article '#{@article.title}' Updated!"
-
-		redirect_to article_path(@article)
-	end
+    redirect_to article_path(@article)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  	resources :articles
-  	#list of indexes became the index page
-  	root to: 'articles#index'
+  resources :articles
+  # list of indexes became the index page
+  root to: 'articles#index'
 end
