b9a8ca0efcf4643e0a0cb00d0cf2ab0f84905f91
laerc <laercio@libercapital.com.br>
2018-02-20 18:37:19 -0300

    Bootstrap feito e adicionando função update

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -9,4 +9,33 @@ class ArticlesController < ApplicationController
 		@article = Article.find(params[:id])
 
 	end
+
+	def new
+		@article = Article.new
+	end
+
+	def create
+		#creates a new article
+		@article = Article.new
+		#takes the values
+  		@article.title = params[:article][:title]
+  		@article.body  = params[:article][:body]
+  		#pull to the database
+  		@article.save
+
+  		#redirects the page
+  		redirect_to article_path(@article)
+	end	
+
+	def destroy
+		Article.find(params[:id]).destroy
+		redirect_to action:"index"
+	end
+	
+	def edit
+		@article = Article.find(params[:id])
+		@article.update(article_params)
+
+		redirect_to article_path(@article)
+	end
 end
